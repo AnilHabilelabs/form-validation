cffb15b43a40e7e195d57f0fd92f4d9cdd57b0cf
Anil Kumar <anil.kumar@habilelabs.io>
2023-01-17 18:43:48 +0530

    updated files

--- js/index.js
+++ js/index.js
@@ -3,21 +3,29 @@ function validation(e) {
   console.log(e);
   let languagesArr = [];
   let radioArr = [];
+  // name validation
   if (e.target.name1.value.includes(" ")) {
     alert("do not use space");
     return false;
   }
 
+  // number validation
   if (isNaN(e.target.mobile.value)) {
     alert("use Numeric value");
     return false;
   }
+  if (e.target.mobile.value.length<10) {
+    alert("Nut 10 values");
+    return false;
+  }
 
+  // Message Validation
   if (e.target.message.value == "") {
     alert("Please Enter Message");
     return false;
   }
 
+  // Radio and CheckBox Validation
   for (i = 0; i < e.target.length; i++) {
     if (e.target[i].type == "checkbox") {
       if (e.target[i].checked) {
